bc0f229549d3a9f5c225caa1fe6f508dde1f47ba
Imanol Fotia <imanolfotia@gmail.com>
2024-08-15 00:35:08 -0300

    Add files via upload

--- arch_efi_install.sh
+++ arch_efi_install.sh
@@ -14,12 +14,31 @@ passwd
 useradd -m solaire
 passwd solaire
 
-pacman -S git neovim wayland plasma plasma-workspace plasma-wayland-protocols hyprland hyprlock xdg-desktop-portal-hyprland grub efibootmgr sddm sudo networkmanager os-prober kitty dolphin
+pacman -S git neovim dhcpcd wayland plasma plasma-workspace plasma-wayland-protocols xdg-desktop-portal-kde grub efibootmgr sddm sudo networkmanager os-prober kitty dolphin nvidia nvidia-utils nvidia-settings opencl-nvidia xorg-server-devel
 
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
+systemctl enable dhcpcd.service
 systemctl enable sddm
 systemctl enable NetworkManager.service
 
 EDITOR=nvim visudo
+
+sudo cp /etc/environment ~/environment.bak
+
+sudo nvim /etc/environment
+  QT_QPA_PLATFORMTHEME="wayland;xcb"
+  GBM_BACKEND=nvidia-drm
+  __GLX_VENDOR_LIBRARY_NAME=nvidia
+  ENABLE_VKBASALT=1
+  LIBVA_DRIVER_NAME=nvidia
+  WLR_NO_HARDWARE_CURSORS=1
+
+sudo nvim /etc/mkinitcpio.conf
+  MODULES(... nvidia nvidia_modeset nvidia_uvm nvidia_drm)
+sudo mkinitcpio -P
+
+sudo nvim /etc/default/grub
+  GRUB_CMDLINE_LINUX_DEFAULT="... nvidia-drm.modeset=1"
+sudo grub-mkconfig -o /boot/grub/grub.cfg
